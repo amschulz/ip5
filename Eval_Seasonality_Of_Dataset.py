c9b4d43595ffc01eed5cf3b2814d1e70b6e57860
Seraina <seraina.brand@studnets.fhnw.ch>
2018-11-09 14:30:47 +0100

    updated eval Seasonality file

--- Eval_Seasonality_Of_Dataset.py
+++ Eval_Seasonality_Of_Dataset.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[10]:
+# In[47]:
 
 
 import pandas as pd
@@ -14,15 +14,20 @@ import math
 import itertools
 import matplotlib.patches as mpatches
 import statistics
-import datetime
+import datetime as dt
 
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 default_startdate = date(2010, 1, 1)
 default_enddate = date(2018, 1, 1)
 
+start = date(2010, 1, 1)
+end = date(2018, 1, 1)
 
-# In[11]:
+path = '../Datenexporte/Datenexport_200818/export_august.txt'
+
+
+# In[46]:
 
 
 def group_by_frequence(df,
@@ -38,8 +43,10 @@ def group_by_frequence(df,
     df = df.reindex(idx, fill_value=0)
     return df
 
+
+
 def get_dataframe(ids_to_track=[]):
-    df = pd.read_csv(filepath_or_buffer='../Datenexporte/Datenexport_200818/export_august.txt',
+    df = pd.read_csv(filepath_or_buffer=path,
                      sep=';',
                      header=0,
                      usecols=[0,6,8,10])
@@ -54,14 +61,18 @@ def get_dataframe(ids_to_track=[]):
     df['Datum'] = pd.to_datetime(df['FaktDatum'], dayfirst=True, errors='raise')
     return df
 
+
+
 # perform Augmented Dickey Fuller test
 # params: training set y, current value for d (count of differencing)
 # returns Tupel of stationary data and d
 def dickey_fuller(data, d):
-    dftest = adfuller(data, autolag='AIC')
-    adf_val = dftest[0]
+    result = adfuller(data, autolag='AIC')
+    adf_val = result[0]
+    if math.isnan(adf_val):
+        return (data, d)
 
-    if adf_val < dftest[4]['5%']:
+    if adf_val < result[4]['5%']:
         # is stationary
         return (data, d)
     else:
@@ -84,12 +95,18 @@ def getSeasonality(series):
     #print(result.resid)
     #print(result.observed)
     
-    seasonal = [round(j) for i, j in enumerate(result.seasonal)]
+    # j*100: since we round the values, we must be sure not to loose too much precision
+    # values for j between 0 and 1 are quite common.
+    seasonal = [round(j*100) for i, j in enumerate(result.seasonal)]
+    # get peaks only
     peak = max(seasonal)
+    # get index of peaks
     res = [i for i, j in enumerate(seasonal) if j == peak]
 
     S = 0
     if len(res)>1 :
+        # taking the first two values only is sufficient, 
+        # the pattern is highly repetitive
         S=res[1]-res[0]
         
     return S
@@ -97,23 +114,51 @@ def getSeasonality(series):
     
 
 
-# In[12]:
+# In[40]:
+
+
+# Code example of decompositioning 
+# prints the four graphs for ONE defined Article
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+from matplotlib import pyplot
+
+#define id to calculate (single examination)
+art_id = [722]
+
+# load and group data to monthly
+grouped = group_by_frequence(get_dataframe(art_id))
+
+# prepare data for training
+series = grouped['Menge']
+
+result = seasonal_decompose(series, model='additive')
+
+# printing out the values for each series
+#print(result.trend)
+#print(result.seasonal)
+#print(result.resid)
+#print(result.observed)
+
+# printing all as charts
+result.plot()
+pyplot.show()
+
+
+# In[44]:
 
 
 # import the data
 
-import datetime as dt
-start = date(2010, 1, 1)
-end = date(2018, 1, 1)
 # define data to calc for (or empty for all)
-my_id =[722, 3986, 7979, 7612, 239, 1060, 5841, 6383, 5830]
+art_id =[] # [722, 3986, 7979, 7612, 239, 1060, 5841, 6383, 5830]
 
 # load data
-df = get_dataframe(my_id)
+df = get_dataframe(art_id)
 print('done import')
 
 
-# In[14]:
+# In[48]:
 
 
 # continue after above code executed:
@@ -153,4 +198,28 @@ print ('total Art.: %i' % len(artId))
 # all => {12: 2815, 1: 694, 2: 75, 3: 49, 4: 34, 5: 30, 7: 26, 8: 21, 6: 18, 9: 14, 10: 5, 11: 4}
 
 # print(results_df)
+
+
+# In[49]:
+
+
+results_df = pd.DataFrame(columns=['Periodic cycle', 'count of articles'])
+for el in counts:
+    results = {'Periodic cycle': el, 'count of articles': counts[el]}
+    results_df = results_df.append(results, ignore_index=True)
+
+
+results_df = results_df.sort_values(['Periodic cycle'], ascending=[1])
+
+
+results_df = results_df.set_index('Periodic cycle')
+# print(results_df)
+results_df.plot()
+pyplot.show()
+
+
+# In[ ]:
+
+
+print(results_df)
 
