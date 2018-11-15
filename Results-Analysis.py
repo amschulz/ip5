12df3a8af6f022f290e68e0a5ad90d8c7b7b08e6
Adrian Schulz <adrian.schulz@students.fhnw.ch>
2018-11-15 18:36:19 +0100

    New version of analysis file.

--- Results-Analysis.py
+++ Results-Analysis.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[97]:
 
 
 import pandas as pd
@@ -21,7 +21,7 @@ from scipy import signal
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 
-# In[3]:
+# In[98]:
 
 
 default_startdate = date(2010, 1, 1)
@@ -74,48 +74,126 @@ def get_quality(test, forecast):
     return {'mape': mape,
             'median': median}
 
+def get_ma(ser, w=2):
+    ma = ser.rolling(window=w).mean()
+    ma = ma.drop(0)
+    ma.index = range(0, len(ma))
+    return ma
+    
 
-# In[4]:
+
+# In[99]:
 
 
 df_read = pd.read_csv(filepath_or_buffer='outputs/SARIMA_Pipeline_2018-11-11_00-58-37.csv',
                  sep=';',
-                 header=0)
-#df_read = results_df
-#df_read['ArtikelID'] = df_read.index
-display(df_read)
-# display(df[(df['RMSE / TrainData Mean'] > 0.3) & (df['RMSE / TrainData Mean'] < 2)])
+                 header=0,
+                 index_col=0)
+df_hortima = pd.read_csv(filepath_or_buffer='outputs/Prognose_Hortima_2018.csv',
+                         sep=';',
+                         header=0,
+                         index_col=0)
+df_read = df_read.drop(columns=['Mape', 'Median', 'TrainData Mean'])
+#display(df_read.head())
+#display(df_hortima.loc[[1744, 1357, 1676, 2355, 4810]])
 
 
-# In[5]:
+# In[100]:
+
+
+for i in range(1,13):
+    df_read['Hortima M%s' % str(i)] = df_hortima[str(i)]
+    
+# display(df_read.head())
+
+
+# In[101]:
+
+
+artIds = df_read.index
+
+median = []
+mape = []
+median_hortima = []
+mape_hortima = []
+ma_median = []
+ma_mape = []
+ma_median_hortima = []
+ma_mape_hortima = []
+drop_col = []
+
+for ind, artId in enumerate(artIds):
+    test_data = []
+    forecast_data = []
+    hortima_data= []
+    for i in range(1, 13):
+        test_data.append(df_read['Test M%s' % i][artId])
+        forecast_data.append(df_read['Forecast M%s' % i][artId])
+        hortima_data.append(df_read['Hortima M%s' % i][artId])
+        if ind == 0:
+            drop_col.append('Test M%s' % i)
+            drop_col.append('Forecast M%s' % i)
+            drop_col.append('Hortima M%s' % i)
+        
+    #if artId == 1744:
+    #    plt.plot(test_data, color='red')
+    #    plt.plot(forecast_data, color='blue')
+    #    plt.plot(hortima_data, color='orange')
+    #    plt.show()
+    
+    # turn lists into pd.series 
+    series_test = pd.Series(test_data)
+    series_forecast = pd.Series(forecast_data)
+    series_hortima = pd.Series(hortima_data)
+    
+    # get quality for actual prediction
+    quality = get_quality(series_test, series_forecast)
+    median.append(quality['median'])
+    mape.append(quality['mape'])
+    
+    quality_hortima = get_quality(series_test, series_hortima)
+    median_hortima.append(quality_hortima['median'])
+    mape_hortima.append(quality_hortima['mape'])
+    
+    # get quality for ma of prediction
+    test_ma2 = get_ma(series_test, 2)
+    forecast_ma2 = get_ma(series_forecast, 2)
+    hortima_ma2 = get_ma(series_hortima, 2)
+    
+    ma_quality = get_quality(test_ma2, forecast_ma2)
+    ma_median.append(ma_quality['median'])
+    ma_mape.append(ma_quality['mape'])
+    
+    ma_quality_hortima = get_quality(test_ma2, hortima_ma2)
+    ma_median_hortima.append(ma_quality_hortima['median'])
+    ma_mape_hortima.append(ma_quality_hortima['mape'])
+
+df_read = df_read.drop(columns=drop_col)
+df_read['MA Median Forecast'] = ma_median
+df_read['MA Mape Forecast'] = ma_mape
+df_read['MA Median Hortima'] = ma_median_hortima
+df_read['MA Mape Hortima'] = ma_mape_hortima
+df_read['Median Forecast'] = median
+df_read['Mape Forecast'] = mape
+df_read['Median Hortima'] = median_hortima
+df_read['Mape Hortima'] = mape_hortima
+df_read[df_read['Skipped']== True]
+df_read[df_read['Error']== True]
+display(df_read.head())
+
+
+# In[102]:
 
 
 print('Analyse')
-boxplot_median = df_read['Median'].dropna()
-boxplot_mape = df_read['Mape'].dropna()
-
-plt.title('Box-Plot des Medians')
-plt.boxplot(boxplot_median, showfliers=False)
-plt.show()
-plt.title('Box-Plot des Mapes')
-plt.boxplot(boxplot_mape, showfliers=False)
-plt.show()
-
-print('Mittelwert des Medians: %s' % boxplot_median.mean())
-print('Mittelwert des Mapes: %s' % boxplot_mape.mean())
-print('Median des Medians: %s' % statistics.median(boxplot_median))
-print('Median des Mapes: %s' % statistics.median(boxplot_mape))
-print('Anzahl übersprungener Artikel: %s' % len(df_read[df_read['Skipped']== True]))
-print('Anzahl Artikel mit Errors: %s' % len(df_read[df_read['Error'] == True]))
-print('Anzahl Artikel mit MAPE über 1.0: %s' % len(boxplot_mape[boxplot_mape > 1.0]))
-print('Anzahl Artikel mit MAPE kleiner oder gleich 1.0: %s' % len(boxplot_mape[boxplot_mape <= 1.0]))
-print('Anzahl Artikel mit MAPE über 0.5: %s' % len(boxplot_mape[boxplot_mape > 0.5]))
-print('Anzahl Artikel mit MAPE kleiner oder gleich 0.5: %s' % len(boxplot_mape[boxplot_mape <= 0.5]))
-print('Anzahl Artikel mit Median über 1.0: %s' % len(boxplot_median[boxplot_median > 1.0]))
-print('Anzahl Artikel mit Median kleiner oder gleich 1.0: %s' % len(boxplot_median[boxplot_median <= 1.0]))
-print('Anzahl Artikel mit Median über 0.5: %s' % len(boxplot_median[boxplot_median > 0.5]))
-print('Anzahl Artikel mit Median kleiner oder gleich 0.5: %s' % len(boxplot_median[boxplot_median <= 0.5]))
-# display(df_read[df_read['Mape'] <= 0.5])
+for col in df_read.columns:
+    if 'Median' in col or 'Mape' in col:
+        an_col = df_read[col]
+        an_col.dropna()
+        plt.title(col)
+        plt.boxplot(boxplot_median, showfliers=False)
+        plt.boxplot(boxplot_median, showfliers=True)
+        plt.show()
 
 
 # In[6]:
@@ -168,77 +246,4 @@ def show_article_details(id=''):
     print('S: %s' % article_df['S'][index])
 
 show_article_details(6673)
-
-
-# In[3]:
-
-
-df1 = pd.read_csv(filepath_or_buffer='outputs/SARIMA_Pipeline_2018-11-11_00-58-37.csv',
-                 sep=';',
-                 header=0)
-artIds = df1['ArtikelID'].unique()
-
-ma_median = []
-ma_mape = []
-
-for index, artId in enumerate(artIds):
-    test_data = []
-    forecast_data = []
-    for i in range(1, 13):
-        test_data.append(df1['Test M%s' % i][index])
-        forecast_data.append(df1['Forecast M%s' %i ][index])
-    series_test = pd.Series(test_data)
-    series_forecast = pd.Series(forecast_data)
-    dfdata = {'mape': df1['Mape'][index], 'median': df1['Median'][index]}
-    test_ma2 = series_test.rolling(window=2).mean()
-    test_ma2 = test_ma2.drop(0)
-    test_ma2.index = range(0, len(test_ma2))
-    forecast_ma2 = series_forecast.rolling(window=2).mean()
-    forecast_ma2 = forecast_ma2.drop(0)
-    forecast_ma2.index = range(0, len(forecast_ma2))
-    #plt.plot(test_data)
-    #plt.plot(forecast_data)
-    #plt.plot(test_ma2)
-    #plt.plot(forecast_ma2)
-    #plt.show()
-    ma_quality = get_quality(test_ma2, forecast_ma2)
-    ma_median.append(ma_quality['median'])
-    ma_mape.append(ma_quality['mape'])
-
-df1['MA Median'] = ma_median
-df1['MA Mape'] = ma_mape
-
-print(df1['Median'].dropna().mean())
-print(df1['MA Median'].dropna().mean())
-print(df1['Mape'].dropna().mean())
-print(df1['MA Mape'].dropna().mean())
-
-
-# In[16]:
-
-
-plt.title('Box-Plot des MA Medians')
-plt.boxplot(df1['MA Median'].dropna(), showfliers=False)
-plt.show()
-plt.title('Box-Plot des MA Mapes')
-plt.boxplot(df1['MA Mape'].dropna(), showfliers=False)
-plt.show()
-
-
-# In[4]:
-
-
-plt.title('Box-Plot des MA Medians')
-plt.boxplot(df1['MA Median'].dropna(), showfliers=True)
-plt.show()
-plt.title('Box-Plot des MA Mapes')
-plt.boxplot(df1['MA Mape'].dropna(), showfliers=True)
-plt.show()
-
-
-# In[6]:
-
-
-display(df1[df1['MA Median'] > 10000])
-display(df1[df1['MA Mape'] > 10000])
 
